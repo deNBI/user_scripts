b23219652c63c95654eaea7ea731affedf02dd01
dweinholz <david-weinholz@web.de>
2021-08-02 12:50:08 +0200

    feat(Bibigrid):updted script to 0.2.0

--- bibigrid/v0/scaling_v0.1.0.py
+++ bibigrid/v0/scaling_v0.1.0.py
@@ -3,23 +3,28 @@ import os
 import re
 import socket
 import sys
+from getpass import getpass
 from pathlib import Path
 
 import requests
 import yaml
 
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 HOME = str(Path.home())
 PLAYBOOK_DIR = HOME + '/playbook'
 PLAYBOOK_VARS_DIR = HOME + '/playbook/vars'
 ANSIBLE_HOSTS_FILE = PLAYBOOK_DIR + '/ansible_hosts'
 INSTANCES_YML = PLAYBOOK_VARS_DIR + '/instances.yml'
 CLUSTER_INFO_URL = "https://cloud.denbi.de/portal/public/clusters/"
+CLUSTER_OVERVIEW = "https://cloud.denbi.de/portal/webapp/#/virtualmachines/clusterOverview"
+WRONG_PASSWORD_MSG = f"The password seems to be wrong. Please verify it again, otherwise you can generate a new one for the cluster on the Cluster Overview ({CLUSTER_OVERVIEW})"
+OUTDATED_SCRIPT_MSG = "Your script is outdated [VERSION: {SCRIPT_VERSION} - latest is {LATEST_VERSION}] -  please download the current script and run it again!"
 
 
 class ScalingDown:
 
-    def __init__(self):
+    def __init__(self, password):
+        self.password = password
         ips = self.get_private_ips()
         valid_ips = []
         for ip in ips:
@@ -38,8 +43,15 @@ class ScalingDown:
 
     def get_private_ips(self):
         global CLUSTER_INFO_URL
-        res = requests.get(url=CLUSTER_INFO_URL, params={"scaling": "scaling_down"})
-        ips = [ip for ip in res.json()["private_ips"] if ip is not None]
+        res = requests.post(url=CLUSTER_INFO_URL,
+                            json={"scaling": "scaling_down", "password": password},
+                            )
+        res = res.json()
+        version = res["VERSION"]
+        if version != VERSION:
+            print(OUTDATED_SCRIPT_MSG.format(SCRIPT_VERSION=VERSION, LATEST_VERSION=version))
+            sys.exit(1)
+        ips = [ip for ip in res["private_ips"] if ip is not None]
         return ips
 
     def validate_ip(self, ip):
@@ -90,7 +102,8 @@ class ScalingDown:
 
 class ScalingUp:
 
-    def __init__(self):
+    def __init__(self, password):
+        self.password = password
         data, valid_ips = self.get_cluster_data()
         if len(data) > 0:
             workers_data = self.create_yml_file(cluster_data=data)
@@ -101,13 +114,23 @@ class ScalingUp:
 
     def get_cluster_data(self):
         global CLUSTER_INFO_URL
-        res = requests.get(url=CLUSTER_INFO_URL, params={"scaling": "scaling_up"})
-        ips = []
 
-        cluster_data = [data for data in  res.json()["active_worker"] if data is not None]
-        for cl in cluster_data:
-            ips.append(cl['ip'])
-        return cluster_data, ips
+        res = requests.post(url=CLUSTER_INFO_URL,
+                            json={"scaling": "scaling_up", "password": password})
+        if res.status_code == 200:
+            res = res.json()
+            version = res["VERSION"]
+            if version != VERSION:
+                print(OUTDATED_SCRIPT_MSG.format(SCRIPT_VERSION=VERSION, LATEST_VERSION=version))
+                sys.exit(1)
+            ips = []
+
+            cluster_data = [data for data in res["active_worker"] if data is not None]
+            for cl in cluster_data:
+                ips.append(cl['ip'])
+            return cluster_data, ips
+        else:
+            print("")
 
     def validate_ip(self, ip):
         print("Validate  IP: ", ip)
@@ -164,6 +187,7 @@ class ScalingUp:
                 out_file.write(line)
 
 
+
 def get_version():
     print("Version: ", VERSION)
 
@@ -187,7 +211,12 @@ if __name__ == '__main__':
         else:
             print("No usage found for param: ", arg)
     else:
+        print("Please enter your cluster password:")
+        password = getpass()
+        if not password:
+            print("Password must not be empty!")
+            sys.exit(1)
         get_cluster_id_by_hostname()
-        ScalingDown()
-        ScalingUp()
+        ScalingDown(password=password)
+        ScalingUp(password=password)
         run_ansible_playbook()
